558f3ba83f55f67b4fdcbc9463ff46dc8f744453
jihad mimouni <Jihadmimouni6@gmail.com>
2023-08-21 23:44:10 +0100

    part 3 done

--- makefile
+++ makefile
@@ -1,36 +1,48 @@
 ASM=nasm
+CC=gcc
 
 SRC_DIR=src
+TOOLS_DIR=tools
 BUILD_DIR=build
 
-.PHONY: all floppy_image kernel bootloader clean always
+.PHONY: all floppy_image kernel bootloader clean always tools_fat
+
+all: floppy_image tools_fat
 
 #
 # Floppy image
 #
 floppy_image: $(BUILD_DIR)/main_floppy.img
+
 $(BUILD_DIR)/main_floppy.img: bootloader kernel
-	dd  if=/dev/zero of=$(BUILD_DIR)/main_floppy.img bs=512 count=2880
+	dd if=/dev/zero of=$(BUILD_DIR)/main_floppy.img bs=512 count=2880
 	mkfs.fat -F 12 -n "NBOS" $(BUILD_DIR)/main_floppy.img
 	dd if=$(BUILD_DIR)/bootloader.bin of=$(BUILD_DIR)/main_floppy.img conv=notrunc
 	mcopy -i $(BUILD_DIR)/main_floppy.img $(BUILD_DIR)/kernel.bin "::kernel.bin"
+
 #
-# boot loader
+# Bootloader
 #
 bootloader: $(BUILD_DIR)/bootloader.bin
 
 $(BUILD_DIR)/bootloader.bin: always
 	$(ASM) $(SRC_DIR)/bootloader/boot.asm -f bin -o $(BUILD_DIR)/bootloader.bin
 
-
-
 #
-#kernel
+# Kernel
 #
 kernel: $(BUILD_DIR)/kernel.bin
 
-$(BUILD_DIR)/kernel.bin: $(BUILD_DIR) $(SRC_DIR)/kernel/main.asm
+$(BUILD_DIR)/kernel.bin: always
 	$(ASM) $(SRC_DIR)/kernel/main.asm -f bin -o $(BUILD_DIR)/kernel.bin
+
+#
+# Tools
+#
+tools_fat: $(BUILD_DIR)/tools/fat
+$(BUILD_DIR)/tools/fat: always $(TOOLS_DIR)/fat/fat.c
+	mkdir -p $(BUILD_DIR)/tools
+	$(CC) -g -o $(BUILD_DIR)/tools/fat $(TOOLS_DIR)/fat/fat.c
 
 #
 # Always
@@ -42,4 +54,4 @@ always:
 # Clean
 #
 clean:
-	rm -rf $(BUILD_DIR)
+	rm -rf $(BUILD_DIR)/*
--- src/kernel/main.asm
+++ src/kernel/main.asm
@@ -1,59 +1,45 @@
-
-org 0x7c00
+org 0x0
 bits 16
 
-%define ENDL 0x0D,0x0A     ;end of line
+
+%define ENDL 0x0D, 0x0A
+
 
 start:
-    
-    jmp main
-
-
-;
-; print string
-; parms:
-;       ds:si -> string
-puts:
-    ;save registers we will modify  
-    push si 
-    push ax
-.loop:
-    lodsb ;load next byte from ds:si into al
-    or al, al ;is it zero?
-    jz .done ;if so, we are done
-    mov ah, 0x0e ;otherwise, print it
-    mov bh, 0x00 ;page number
-    int 0x10
-    jmp .loop ;and repeat
-.done:
-    pop ax
-    pop si
-    ret
-
-
-main:
-    ; setup data segment
-    mov ax, 0 ;can't write to ds and es directly
-    mov ds, ax
-    mov es, ax
-
-    ; setup stack
-    mov ss, ax
-    mov sp, 0x7c00 ;stack grows down
-
-    ; print message
+    ; print hello world message
     mov si, msg_hello
     call puts
-    
-    hlt
 
 .halt:
-    jmp .halt
+    cli
+    hlt
 
-msg_hello: db 'Hello, World! ',ENDL, 0
+;
+; Prints a string to the screen
+; Params:
+;   - ds:si points to string
+;
+puts:
+    ; save registers we will modify
+    push si
+    push ax
+    push bx
 
+.loop:
+    lodsb               ; loads next character in al
+    or al, al           ; verify if next character is null?
+    jz .done
 
-times 510 - ($ - $$) db 0
-dw 0AA55h
+    mov ah, 0x0E        ; call bios interrupt
+    mov bh, 0           ; set page number to 0
+    int 0x10
 
+    jmp .loop
 
+.done:
+    pop bx
+    pop ax
+    pop si    
+    ret
+
+msg_hello: db 'Hello world from KERNEL!', ENDL, 0
